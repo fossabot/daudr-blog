0b8a3ef6ace95189dd7c32694bb01cfed58f35dc
Michele Da Rin Fioretto <michele@daudr.me>
2019-07-08 09:41:33 +0200

    feat(unit-testing): Add beforeEach()

--- src/components/email-signup/email-signup.spec.js
+++ src/components/email-signup/email-signup.spec.js
@@ -2,8 +2,17 @@ import React from "react"
 import renderer from "react-test-renderer"
 
 import EmailSignup from "./email-signup"
+import { testHook } from "../../utils/test-utils";
+
+let invalidEmail
 
 describe("EmailSignup", () => {
+  beforeEach(() => {
+    testHook(() => {
+      
+    })
+  })
+
   it("renders correctly", () => {
     const tree = renderer
       .create(<EmailSignup />)
