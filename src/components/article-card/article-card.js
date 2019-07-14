82cf69cf4947f42b0618fb28fb0e31784ff18a2b
Michele Da Rin Fioretto <michele@daudr.me>
2019-07-14 16:16:04 +0200

    style(article-card): style ArticleCard's tags

--- src/components/article-card/article-card.js
+++ src/components/article-card/article-card.js
@@ -5,18 +5,23 @@ import { makeStyles } from "@material-ui/styles"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
-import { rhythm } from "../../utils/typography";
+import Chip from "@material-ui/core/Chip"
+import { rhythm } from "../../utils/typography"
+import kebabCase from "lodash/kebabCase"
 
 const useStyles = makeStyles({
   card: {
     minWidth: 275,
-    marginBottom: rhythm(1 / 2)
+    marginBottom: rhythm(1 / 2),
   },
   title: {
     fontSize: 14,
   },
   pos: {
     marginBottom: 12,
+  },
+  chip: {
+    margin: `2px`,
   },
 })
 
@@ -32,16 +37,34 @@ export const ArticleCard = ({ node }) => {
           color="textSecondary"
           gutterBottom
         >
-          {/* TODO: Aggiungere i veri tag dell'articolo
-          */}
-          #tag #tag #tag
+          {node.frontmatter.tags.slice(0, 3).map(tag => {
+            return (
+              <Link
+                key={tag}
+                to={`/tags/${kebabCase(tag)}`}
+                style={{ boxShadow: `none` }}
+              >
+                <Chip
+                  size="small"
+                  label={`#${tag}`}
+                  className={classes.chip}
+                  onClick={() => {}}
+                ></Chip>
+              </Link>
+            )
+          })}
         </Typography>
         <Typography variant="h5" component="h2" data-cy="post-title">
           <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
             {title}
           </Link>
         </Typography>
-        <Typography className={classes.pos} variant="subtitle2" component="p" color="textSecondary">
+        <Typography
+          className={classes.pos}
+          variant="subtitle2"
+          component="p"
+          color="textSecondary"
+        >
           {node.frontmatter.date}
         </Typography>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -51,6 +51,7 @@ export const pageQuery = graphql`
             date(formatString: "MMMM DD, YYYY")
             title
             description
+            tags
           }
         }
       }
